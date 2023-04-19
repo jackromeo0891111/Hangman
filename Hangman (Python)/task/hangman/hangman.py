92702fb87b182802786591b3da787af9b8921e5c
Jack Yeh <jackhyyeh@gmail.com>
2023-04-18 17:17:06 -0700

    .

--- Hangman (Python)/task/hangman/hangman.py
+++ Hangman (Python)/task/hangman/hangman.py
@@ -4,21 +4,12 @@ import random
 print("H A N G M A N")
 answers = ["python", "java", "swift", "javascript"]
 answer = random.choice(answers)
-updated_word = ""
-hidden_word = "-" * len(answer)
-print(hidden_word)
-attempt = 0
-while attempt < 8:
+letters = dict({c: '-' for c in answer})
+for _ in range(8):
+    print("\n" + "".join(letters[c] for c in answer))
     guess = input("Input a letter:")
-    if guess in answer:
-        for letter in answer:
-            if letter == guess:
-                hidden_word = hidden_word.replace("-", letter)
-            else:
-                hidden_word = hidden_word.replace("-", "-")
-        print(hidden_word)
-        attempt += 1
+    if guess in letters:
+        letters[guess] = guess
     else:
         print("That letter doesn't appear in the word.")
-        attempt += 1
 print("Thanks for playing!")
